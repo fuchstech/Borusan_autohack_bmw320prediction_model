d388cc191d18d90ece44035f27274db9c3bdd05b
Kerem Yıldız <49817710+fuchstech@users.noreply.github.com>
2023-12-17 20:38:11 +0300

    trying different methods

--- Car Price Dataset/codes/cnn_location_import.py
+++ Car Price Dataset/codes/cnn_location_import.py
@@ -35,6 +35,29 @@ paint_values = {
     'Tailgate': 1,
     'Roof': 3
 }
+group_0_cities = ["Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya", "Ankara", "Antalya", "Ardahan", 
+                  "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", 
+                  "Bitlis", "Bolu", "Burdur", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Elazığ", 
+                  "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", 
+                  "Iğdır", "Isparta", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", 
+                  "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Konya", "Kütahya", "Malatya", "Manisa", 
+                  "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", 
+                  "Samsun", "Siirt", "Sinop", "Sivas", "Şanlıurfa", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", 
+                  "Tunceli", "Uşak", "Van", "Yozgat", "Zonguldak"]
+
+group_1_cities = ["Bursa", "Kocaeli", "Sakarya", "Yalova", "Tekirdağ", "Edirne", "Çanakkale", "İzmir"]
+
+# location sütununu dönüştürecek fonksiyon
+def transform_location(location):
+    if location in group_0_cities:
+        return 0
+    elif location in group_1_cities:
+        return 1
+    else:
+        return None  # Eğer listede yoksa None değerini döndürür (veya başka bir değer atayabilirsiniz)
+
+# Uygulama
+bmw_data['location_transformed'] = bmw_data['location'].apply(transform_location)
 
 # Function to calculate the total value for replacements and paints
 def calculate_total_value(column, value_dict):
@@ -46,73 +69,53 @@ def calculate_total_value(column, value_dict):
             total_value += value_dict.get(item, 0)  # Add the value, default to 0 if not found
     return total_value
 
-# Applying the transformations to the dataset
 bmw_data['replacement_value'] = bmw_data['replacements'].apply(lambda x: calculate_total_value(x, replacement_values))
 bmw_data['paint_value'] = bmw_data['paints'].apply(lambda x: calculate_total_value(x, paint_values))
 
-# Selecting relevant features and the target variable
-features = ['replacement_value', 'paint_value', 'milage', 'year', 'crash cost']
+# 'location_transformed' özelliğini de dahil ediyoruz
+features = ['replacement_value', 'paint_value', 'milage', 'year', 'crash cost', 'location_transformed']
 target = 'price'
 
-# Dropping rows with missing target values and handling any missing values in features
+# Verileri temizleme ve özellik değerlerini doldurma
 bmw_data_clean = bmw_data.dropna(subset=[target])
 X = bmw_data_clean[features].fillna(bmw_data_clean[features].median())
 y = bmw_data_clean[target]
 
-# Splitting the dataset into training and testing sets
+# Veriyi eğitim ve test setlerine ayırma
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Scaling the features
+# Özellikleri ölçeklendirme
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Neural Network Design
+# Sinir Ağı Dizaynı
 model = Sequential()
 model.add(Dense(128, activation='relu', input_shape=(X_train_scaled.shape[1],)))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
-model.add(Dense(1, activation='linear'))  # Output layer for regression
+model.add(Dense(1, activation='linear'))  # Regresyon için çıkış katmanı
 
-# Compile the model
+# Modeli derleme
 model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
 early_stopping = EarlyStopping(monitor='val_loss', patience=30, verbose=1)
-# Training the model
-history = model.fit(X_train_scaled, y_train, validation_split=0.2, epochs=100, batch_size=32, verbose=1, callbacks = [early_stopping])
 
-# Evaluate the model on the test set
+# Modeli eğitme
+history = model.fit(X_train_scaled, y_train, validation_split=0.2, epochs=100, batch_size=32, verbose=1, callbacks=[early_stopping])
+
+# Modeli test setinde değerlendirme
 test_loss = model.evaluate(X_test_scaled, y_test, verbose=1)
-print(f'Test Loss: {test_loss}')
+print(f'Test Kaybı (MSE): {test_loss}')
 
-
-#**********************************************************************************************************************
-#Plot predictions vs actual
-import matplotlib.pyplot as plt
-import numpy as np
+# Test seti için tahminler üretme
 y_pred_nn = model.predict(X_test_scaled).flatten()
 
-# Selecting a subset of data for plotting for clarity
-sample_size = 20  # Adjust as needed
-indices = np.arange(sample_size)
-actual_sample = y_test.iloc[:sample_size].to_numpy()
-predicted_sample = y_pred_nn[:sample_size]
-
-# Plotting the actual vs predicted prices
-plt.figure(figsize=(12, 6))
-plt.plot(indices, actual_sample, label='Actual Prices', marker='o', color='b')
-plt.plot(indices, predicted_sample, label='Predicted Prices', marker='x', color='r')
-plt.xlabel('Sample Index')
-plt.ylabel('Price')
-plt.title('Actual vs Predicted Prices Comparison')
-plt.legend()
-plt.show()
-
+# [Ek metriklerin hesaplanması ve çizim kodları aynı kalıyor]
 mae = mean_absolute_error(y_test, y_pred_nn)
 mse = test_loss  # Mean Squared Error
 rmse = mse ** 0.5  # Root Mean Squared Error
 r2 = r2_score(y_test, y_pred_nn)  # R-squared
-
-print(f'Test Loss (MSE): {test_loss}')
-print(f'Mean Absolute Error: {mae}')
-print(f'Root Mean Squared Error: {rmse}')
-print(f'R-squared: {r2}')
+# Değerlendirme metriklerini yazdırma
+print(f'Ortalama Mutlak Hata (MAE): {mae}')
+print(f'Kök Ortalama Kare Hata (RMSE): {rmse}')
+print(f'R-kare: {r2}')
